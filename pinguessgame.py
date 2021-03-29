fae635bc27c9f6c84db61c9227a04609b820c2c1
MontyMango <59061157+MontyMango@users.noreply.github.com>
2021-03-28 21:12:08 -0400

    day zero patch
    The main files showed answers and cheats were enabled for debugging purposes, this will replace that file.
    The debug file will be included in a later branch.

--- pinguessgame.py
+++ pinguessgame.py
@@ -1,4 +1,3 @@
-from sys import path
 from random import randrange
 from time import sleep
 
@@ -12,7 +11,7 @@ class pin():
         self.inp1 = 0
         self.lst1 = []
         
-        # Count down
+        # Moves
         self.counter = 5
         
         # Correct numbers
@@ -26,24 +25,22 @@ class pin():
         
     def switchnums(self):
         self.n = randrange(1000,9999)   # Random number from 1000 - 9999
-        print(self.n)
         self.n = str(self.n)
         for l in self.n:                # Coverts number into list
           self.lst.append(l)
-          print(self.lst)
         self.counter = 5                # Sets counter back to 5
-        print("\n\n\nReady to play!\n\n\n")
+        print("Ready to play!\n\n")
         self.guess()
 
     def guess(self):
-        self.inp1 = input("Put in pin guess: ")  # Asks for a number
+        self.inp1 = input("\nPut in pin guess: ")  # Asks for a number
         for f in self.inp1:                           # Coverts guess into list
             self.lst1.append(f)
-            print(self.lst1)
         self.check()
           
 
     def check(self):
+      print("\nResults:")
       for i in range(0,4):    # Changes number
         if i == 0:
           self.nmbr = self.n1
@@ -53,15 +50,15 @@ class pin():
           self.nmbr = self.n3
         elif i == 3:
           self.nmbr = self.n4
-          
+
         if self.lst[i] == self.lst1[i]:
-            print(i+1," Correct!\n")
+            print("#",i+1," Correct!")
             self.nmbr = 1
         elif self.lst[i] < self.lst1[i]:
-            print(i+1," Less\n")
+            print("#",i+1," Less")
             self.nmbr = 0
         elif self.lst[i] > self.lst1[i]:
-            print(i+1," More\n")
+            print("#",i+1," More")
             self.nmbr = 0
         
         if i == 0:
@@ -74,27 +71,29 @@ class pin():
           self.n4 = self.nmbr
           
       self.counter-=1         # Counter minus one
-      
+
+      if self.n1 and self.n2 and self.n3 and self.n4 == 1:  # If you win
+          print("\nWow, you're so good at this.")
+          sleep(5)
+          print("But lets see if you are good at this one...")
+          sleep(1)
+          print("Generating new pin...")
+          sleep(1)
+          del self.lst1[0:4]    # deletes guessing list
+          del self.lst[0:4]     # deletes automated list
+          self.switchnums()
+
       if self.counter == 0:   # If you run out of guesses
         print("Game over...")
         sleep(2)
         print("The correct numbers for this is ",self.n)
         sleep(3)
         print("Restarting list...")
+        sleep(1)
         del self.lst[0:4]
         self.switchnums()
-
-      if self.n1 and self.n2 and self.n3 and self.n4 == 1:
-          print("\nWow, you're so good at this.")
-          sleep(5)
-          print("Generating new puzzle...")
-          sleep(1)
-          del self.lst1[0:4]    # deletes guessing list
-          del self.lst[0:4]     # deletes automated list
-          self.switchnums()
       else:
         del self.lst1[0:4]      # deletes guessing list
-        print(self.lst1)
         self.guess()          # If not, take another guess
 
 obj = pin()
