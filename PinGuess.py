b2ffc6701d79eb74a0702fa3d983505616f629c2
MontyMango <59061157+MontyMango@users.noreply.github.com>
2021-04-28 10:45:09 -0400

    Scoreboard added
    The scoreboard doesn't save when the application is killed. There's also no error checking so, this application is still prone to crashing.

--- PinGuess.py
+++ PinGuess.py
@@ -6,26 +6,31 @@ class pin():
         # Guessing strings
         self.n = 0000
         self.lst = []
-        
+
         # Input Strings
         self.inp1 = 0
         self.lst1 = []
-        
+
         # Count down
         self.counter = 5
-        
+
         # Correct numbers
         self.n1 = 0
         self.n2 = 0
         self.n3 = 0
         self.n4 = 0
-        
+
         # for the Checker
         self.nmbr = 0
-        
+
         # debug (-1 to activate)
         self.dbg = 0
-        
+
+        # win / loss count
+        self.win = 0
+        self.loss = 0
+        # self.ratio = self.win / self.loss
+
     def toggledebugmode(self):
       if self.dbg == 0:
         self.dbg = 1
@@ -34,7 +39,7 @@ class pin():
         self.dbg = 0
         print("Debug mode off")
       self.guess()
-      
+
     def switchnums(self):
         self.n = randrange(1000,9999)   # Random number from 1000 - 9999
         if self.dbg == 1:
@@ -57,7 +62,7 @@ class pin():
             if self.dbg == 1:
               print(self.lst1)      # Checks if numbers made it on list
         self.check()
-          
+
 
     def check(self):
       print("\nResults:")
@@ -70,7 +75,7 @@ class pin():
           self.nmbr = self.n3
         elif i == 3:
           self.nmbr = self.n4
-          
+
         if self.lst[i] == self.lst1[i]:
             print("#",i+1," Correct!")
             self.nmbr = 1
@@ -80,7 +85,7 @@ class pin():
         elif self.lst[i] > self.lst1[i]:
             print("#",i+1," More")
             self.nmbr = 0
-        
+
         if i == 0:
           self.n1 = self.nmbr
         elif i == 1:
@@ -89,12 +94,15 @@ class pin():
           self.n3 = self.nmbr
         elif i == 3:
           self.n4 = self.nmbr
-          
+
       self.counter-=1         # Counter minus one
 
       if self.n1 and self.n2 and self.n3 and self.n4 == 1:  # If you win
           print("\nWow, you're so good at this.")
           sleep(5)
+          self.win+=1
+          self.score()
+          sleep(2)
           print("But lets see if you are good at this one...")
           sleep(1)
           print("Generating new pin...")
@@ -107,6 +115,9 @@ class pin():
         print("Game over...")
         sleep(2)
         print("The correct numbers for this is ",self.n)
+        sleep(2)
+        self.loss+=1
+        self.score()
         sleep(3)
         print("Restarting list...")
         del self.lst[0:4]
@@ -116,6 +127,10 @@ class pin():
         if self.dbg == 1:
           print(self.lst1)        # prints and checks if list is deleted
         self.guess()          # If not, take another guess
+
+    def score(self):        # Prints scoreboard
+        print("\nScoreboard\n","_"*10,"Wins:",self.win,"\nLosses:",self.loss,"\n")
+
 
 obj = pin()
 obj.switchnums()
