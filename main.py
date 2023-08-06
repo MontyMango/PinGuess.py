f88af1b3426bdc4e2d673fee80e7defa2d74cddf
Cam <59061157+MontyMango@users.noreply.github.com>
2023-08-06 16:05:09 +0000

    Big QOL to code Everything should work, and be easier to read. ### Variables: + debug variable has turned into a bool variable + Some variables have been removed due to not being useful
    ### Functions:
    + check function has been shortened and re-written.
    + toggledebugmode is shorter and re-written.
    
    ### QOL improvements:
    + Guesses left are now displayed
    + Quitting hint is now displayed everytime a new number is generated
    + Keyboardinterrupt has been added to quit the program efficiently

--- main.py
+++ main.py
@@ -19,83 +19,90 @@ class pin():
         self.inp1 = 0
         self.lst1 = []
 
-        # Correct numbers
-        self.n1 = 0
-        self.n2 = 0
-        self.n3 = 0
-        self.n4 = 0
-
-        # for the Checker
-        self.nmbr = 0
-
         # debug (-1 to activate)
-        self.dbg = 0
+        self.debug_mode_on = False
 
 
         # self.ratio = self.win / self.loss
 
     def toggledebugmode(self):
-      if self.dbg == 0:
-        self.dbg = 1
-        print("Debug mode on")
-      elif self.dbg == 1:
-        self.dbg = 0
+      if self.debug_mode_on:
+        self.debug_mode_on = False
         print("Debug mode off")
+      else:
+        self.debug_mode_on = True
+        print("Debug mode on")
       self.guess()
+
 
     def switchnums(self):
         self.n = randrange(1000,9999)   # Random number from 1000 - 9999
         
-        if self.dbg == 1:
-          print(self.n)                   # Shows random number
+        if self.debug_mode_on:
+          print(self.n)                 # Shows answer
         self.n = str(self.n)
         
         for l in self.n:                # Coverts number into list
           self.lst.append(l)
-          if self.dbg == 1:
+          if self.debug_mode_on:
             print(self.lst)
+
         self.counter = 5                # Sets counter back to 5
-        print("Ready to play!\n\n")
-        self.guess()
+        print("Ready to play! If you want to quit, do Ctrl + C to exit the game\n\n")
+
 
     def guess(self):
-        self.inp1 = input("\nPut in pin guess: ")  # Asks for a number
-        if self.inp1 == "-1":
-          self.toggledebugmode()
-        for f in self.inp1:         # Coverts guess into list
-            self.lst1.append(f)
-            if self.dbg == 1:
-              print(self.lst1)      # Checks if numbers made it on list
+      print("\n\nGuesses Left: " + str(self.counter))
+      self.inp1 = input("Put in pin guess: ")  # Asks for a number
+      if self.inp1 == "-1":
+        self.toggledebugmode()
+      elif self.inp1 == "-2":
+        self.show_answer()
+      # elif self.inp1.index == 4:
+      else:
+        self.put_num_into_list()
         self.check()
+      # else:
+        # print("You need to input a 4 pin number")
+
+
+    def put_num_into_list(self):
+      for f in self.inp1:         # Coverts guess into list
+            self.lst1.append(f)
+            if self.debug_mode_on:
+              print(self.lst1)      # Checks if numbers made it on list
+      
+    def show_answer(self):
+      if self.debug_mode_on:
+        print(self.n)
+      else:
+        print("You cheater! You are not going to get the answer!")
 
     # Checks the guessed number
     def check(self):
       print("\nResults:")
       try:
-        for i in range(0,4):    # Changes number
-          if i == 0: self.nmbr = self.n1
-          elif i == 1: self.nmbr = self.n2
-          elif i == 2: self.nmbr = self.n3
-          elif i == 3: self.nmbr = self.n4
-          if self.lst[i] == self.lst1[i]:
-            print("#",i+1," Correct!")
-            self.nmbr = 1
-          elif self.lst[i] < self.lst1[i]:
-            print("#",i+1," Less")
-            self.nmbr = 0
-          elif self.lst[i] > self.lst1[i]:
-            print("#",i+1," More")
-            self.nmbr = 0
-          if i == 0: self.n1 = self.nmbr
-          elif i == 1: self.n2 = self.nmbr
-          elif i == 2: self.n3 = self.nmbr
-          elif i == 3: self.n4 = self.nmbr
+        total_correct = 0      
+        for i in range(1,5):    # Changes number
+          number_on_list = i - 1
+          
+          if self.lst[number_on_list] == self.lst1[number_on_list]:
+            print("#", i," Correct!")
+            total_correct+=1
+          elif self.lst[number_on_list] < self.lst1[number_on_list]:
+            print("#", i," Less")
+          elif self.lst[number_on_list] > self.lst1[number_on_list]:
+            print("#", i," More")
+
+          if self.debug_mode_on:
+            print(total_correct)
+
 
         del self.lst1[0:]
         self.counter-=1         # Counter minus one
 
 
-        if self.n1 and self.n2 and self.n3 and self.n4 == 1:  # If you win
+        if (total_correct == 4):  # If you win
           print("\nWow, you're so good at this.")
           sleep(5)
           self.win+=1
@@ -109,7 +116,7 @@ class pin():
           del self.lst[0:4]     # deletes automated list
           self.switchnums()
 
-        if self.counter == 0:   # If you run out of guesses
+        elif self.counter == 0:   # If you run out of guesses
           print("Game over...")
           sleep(2)
           print("The correct numbers for this is ",self.n)
@@ -120,22 +127,34 @@ class pin():
           print("Restarting list...")
           del self.lst[0:4]
           self.switchnums()
+
         else:
           del self.lst1[0:4]      # deletes guessing list
-          if self.dbg == 1:
+          if self.debug_mode_on:
             print(self.lst1)        # prints and checks if list is deleted
-          self.guess()          # If not, take another guess
-      except IndexError:
+          self.guess()              # If not, take another guess
+
+      # User didn't put enough numbers for the pin
+      except IndexError:          
         print("\nPlease use a 4 number guess.\n")
         del self.lst1[0:]   # deletes input guessing list to start a new one
         self.guess()
+      
+      # User wants to quit
+      except KeyboardInterrupt:   
+        print("\nThanks for playing!")
+        exit
+
       except:
         print("We don't know what happened...")
         self.guess()
+
 
     def score(self):        # Prints scoreboard
         print("\nScoreboard\n","_"*10,"\n Wins:",self.win,"\n Losses:",self.loss,"\n","_"*10)
 
 
 obj = pin()
-obj.switchnums()
+while __name__ == '__main__':
+  obj.switchnums()
+  obj.guess()
